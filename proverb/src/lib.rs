50493c37cd4a0e135e0d3bd37004db8530ba901c
Shohei Jinno <shohei.jinno@gmail.com>
2020-12-19 22:49:59 -0800

    Finish proverb exercise.

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,41 @@
+// pub fn build_proverb(list: &[&str]) -> String {
+//     let num_of_inputs = list.len();
+
+//     if num_of_inputs == 0 {
+//         return String::new();
+//     }
+
+//     let last_proverb = format!("And all for the want of a {}.", list[0]);
+
+//     if num_of_inputs == 1 {
+//         return last_proverb;
+//     }
+
+//     let mut rhyme = String::new();
+//     for i in 0..num_of_inputs - 1 {
+//         rhyme.push_str(&format!(
+//             "For want of a {} the {} was lost.\n",
+//             list[i],
+//             list[i + 1]
+//         ));
+//     }
+//     rhyme.push_str(&last_proverb);
+
+//     rhyme
+// }
+
+// Cool solution...
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    if list.is_empty() {
+        return String::new();
+    }
+
+    list.windows(2)
+        .map(|win| format!("For want of a {0} the {1} was lost.", win[0], win[1]))
+        .chain(std::iter::once(format!(
+            "And all for the want of a {}.",
+            list[0]
+        )))
+        .collect::<Vec<_>>()
+        .join("\n")
 }
