bc67cff924b8e6bf172b5d8c1f5a714e456df6f6
Shohei Jinno <shohei.jinno@gmail.com>
2020-12-29 12:54:50 -0800

    Fibonacci sequence using successors.

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,22 +1,27 @@
+use std::iter::{from_fn, successors};
+
 fn main() {
     // Fibonacci sequence using scan.
-    let i = (0..10).scan((0, 1), |s, _| {
+    let i = (0..).scan((0, 1), |s, _| {
         let f = s.0;
         *s = (s.1, s.0 + s.1);
         Some(f)
     });
-    println!("{:?}", i.collect::<Vec<_>>());
+    println!("{:?}", i.take(6).collect::<Vec<_>>());
 
     // Fibonacci sequence using from_fn.
     let mut f = (0, 1);
-    let fib = std::iter::from_fn(move || {
+    let fib = from_fn(move || {
         let n = f.0;
         f = (f.1, f.0 + f.1);
-        if n < 42 {
-            Some(n)
-        } else {
-            None
-        }
+        Some(n)
     });
-    println!("{:?}", fib.collect::<Vec<_>>());
+    println!("{:?}", fib.take(20).collect::<Vec<_>>());
+
+    // Fibonacci sequence using successors.
+    let fibbb = successors(Some((0, 1)), |&(prev, current)| {
+        Some((current, current + prev))
+    })
+    .map(|(prev, _)| prev);
+    println!("{:?}", fibbb.take(20).collect::<Vec<_>>());
 }
