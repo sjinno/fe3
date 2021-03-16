397424f8d2afc6d0c75a3755b85ec5c2295d229c
Shohei Jinno <shohei.jinno@gmail.com>
2021-03-16 15:29:03 -0700

    Practicing macros.

--- src/bin/macros.rs
+++ src/bin/macros.rs
@@ -1,3 +1,15 @@
+use termion::color::Green;
+
+// Formats string with color.
+// Requires `termion` crate.
+#[macro_export]
+macro_rules! color {
+    ($c:expr, $val:expr) => {{
+        use termion::color::{Fg, Reset};
+        format!("{c1}{}{c0}", $val, c1 = Fg($c), c0 = Fg(Reset))
+    }};
+}
+
 #[macro_export]
 macro_rules! printf {
     ($v:expr) => {
@@ -5,4 +17,7 @@ macro_rules! printf {
     };
 }
 
-fn main() {}
+fn main() {
+    let g = color!(Green, "●");
+    println!("{}", g);
+}
